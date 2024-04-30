ea7c232aa577e87f9b513820a884068ad1766030
suryanshm000 <suryanshm000@gmail.com>
2024-04-30 23:51:28 +0530

    Add blog model, routes and controller

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
+const dotenv = require("dotenv").config();
 
 // express app
 const app = express();
 
-app.listen(3000, () => console.log("app is running at port 3000"))
+// connect to mongodb & listen for requests
+mongoose.connect(process.env.CONNECTION_STRING)
+  .then(result => app.listen(3000, () => console.log(`Database is connected and app is running at http://localhost:3000`)))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 
 // routes
 app.get('/', (req, res) => {
-  res.send("Hello this is blog content app")
+  res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
+});
+
+// blog routes
+app.use('/blogs', blogRoutes);
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
 });
